689d0b3a6fe169abc59b3a2af38e1fcaa0a1e415
isahh <isaah3823@gmail.com>
2024-11-25 07:45:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'ASTRONOMIA',
+    'Qual é o maior planeta do sistema solar?',
+    'JÚPITER.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem na Terra?',
+    'SETE.'
+)
+
+criaCartao(
+    'QUÍMICA',
+    'Qual é o metal conhecido por ser o mais leve?',
+    'LÍTIO.'
+)
+
+criaCartao(
+    'LITERATURA',
+    'Quem escreveu Romeu e Julieta?',
+    'WILLIAM SHAKESPEARE.'
+)
